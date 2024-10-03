1bbaee87b58892dc0f17ca6d14eb75b08d6d6ed0
YUREKAN1 <144401566+YUREKAN1@users.noreply.github.com>
2024-10-03 16:20:17 +0530

    Update graph_bfs.py

--- GRAPHS/graph_bfs.py
+++ GRAPHS/graph_bfs.py
@@ -1,17 +1,46 @@
-from collections import deque
+
+class Graph:
+    def __init__(self):
+        self.graph={}
+        self.directed=False
+    def addVertex(self,vertex):
+        if vertex not in self.graph:
+            self.graph[vertex]=[] 
+
+    def addEdge(self,src,dest):
+        
+        self.addVertex(src)
+        self.addVertex(dest)
+
+        self.graph[src].append(dest)
+        if not self.directed:
+            self.graph[dest].append(src)
+    def displaygraph(self):
+        for vertex in self.graph:
+            print(f"{vertex} ->{self.graph[vertex]}")
 
 
-def bfs(graph, start):
-    visited = set()
-    queue = deque([start])
-    visited.add(start)
+    def bfs(self,startvertex):
+        queue=[startvertex]
+        visited={startvertex:True}
+        while queue:
+            currentvertex=queue.pop(0)
+            print(currentvertex,end=" ")
+            for i in self.graph[currentvertex]:
+                if i not in visited:
+                    queue.append(i)
+                    visited[i]=True
 
-    while queue:
-        current_vertex = queue.popleft()
-        print(current_vertex, end=' ')
+A=Graph()
+A.addEdge('A','B')
+A.addEdge('A','D')
+A.addEdge('A','C')
+A.addEdge('C','D')
+A.addEdge('D','E')
+A.addEdge('C','F')
+A.addEdge('E','F')
+A.addEdge('D','B')
 
-        for neighbor in graph[current_vertex]:
-            if neighbor not in visited:
-                visited.add(neighbor)
-                queue.append(neighbor)
+A.displaygraph()
+A.bfs('A')
 
