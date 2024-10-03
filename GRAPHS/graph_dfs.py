3c7a004372e14241a339813ae7d13255a747b076
YUREKAN1 <144401566+YUREKAN1@users.noreply.github.com>
2024-10-03 16:22:02 +0530

    Update graph_dfs.py

--- GRAPHS/graph_dfs.py
+++ GRAPHS/graph_dfs.py
@@ -1,11 +1,40 @@
-def dfs(graph, start, visited=None):
-    if visited is None:
-        visited = set()
+class Graph:
+    def __init__(self):
+        self.graph={}
+        self.directed=False
+    def addVertex(self,vertex):
+        if vertex not in self.graph:
+            self.graph[vertex]=[] 
 
-    print(start, end=' ')
-    visited.add(start)
+    def addEdge(self,src,dest):
+        
+        self.addVertex(src)
+        self.addVertex(dest)
 
-    for neighbor in graph[start]:
-        if neighbor not in visited:
-            dfs(graph, neighbor, visited)
+        self.graph[src].append(dest)
+        if not self.directed:
+            self.graph[dest].append(src)
+    def displaygraph(self):
+        for vertex in self.graph:
+            print(f"{vertex} ->{self.graph[vertex]}")
+
+    def dfs(self,vertex,visited):
+        if vertex not in visited:
+            print(vertex,end=' ')
+            visited[vertex]=True
+            for v in self.graph[vertex]:
+                self.dfs(v,visited)    
+
+A=Graph()
+A.addEdge('A','B')
+A.addEdge('A','D')
+A.addEdge('A','C')
+A.addEdge('C','D')
+A.addEdge('D','E')
+A.addEdge('C','F')
+A.addEdge('E','F')
+A.addEdge('D','B')
+
+A.displaygraph()
+A.dfs('A',{})
 
